651734b989553f75eea6530b5edadee1fe9fc425
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-27 14:39:59 -0300

    Added method editContact in ContactsAPI class

--- src/api/contactsAPI.js
+++ src/api/contactsAPI.js
@@ -1,42 +1,52 @@
 import axios from 'axios';
 
 class ContactsAPI {
-    constructor(baseURL, timeout) {
-        this.api = axios.create({
-            baseURL,
-            timeout,
-        });
-    }
+	constructor(baseURL, timeout) {
+		this.api = axios.create({
+			baseURL,
+			timeout,
+		});
+	}
 
-    async getAllContacts(token) {
-        const { data } = await this.api.get('/contacts', {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+	async getAllContacts(token) {
+		const { data } = await this.api.get('/contacts', {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
 
-        return data;
-    }
+		return data;
+	}
 
-    async getContactById(id, token) {
-        const { data } = await this.api.get(`/contacts/${id}`, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
+	async getContactById(id, token) {
+		const { data } = await this.api.get(`/contacts/${id}`, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
 
-        console.log(data);
-    }
+		console.log(data);
+	}
 
-    async createNewContact(contactData, token) {
-        const { data } = await this.api.post('/contacts', contactData, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+	async createNewContact(contactData, token) {
+		const { data } = await this.api.post('/contacts', contactData, {
+			headers: {
+				Authorization: `Bearer ${token}`
+			}
+		});
 
-        return data;
-    }
+		return data;
+	}
+
+	async editContact(newData, id, token) {
+		const { data } = await this.api.put(`/contacts/${id}`, newData, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		return data;
+	}
 }
 
 export default ContactsAPI;
