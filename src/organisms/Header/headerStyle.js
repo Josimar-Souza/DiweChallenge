bb86fc88b2afdbc5a299eef7b9530c442b451003
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-28 17:07:29 -0300

    Added style to Header organism

--- src/organisms/Header/headerStyle.js
+++ src/organisms/Header/headerStyle.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
-const HeaderStyle = styled.div``;
+const HeaderStyle = styled.div`
+  align-items: center;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  background: white;
+  display: flex;
+  height: 4rem;
+  padding: 10px;
+`;
 
 export default HeaderStyle;
