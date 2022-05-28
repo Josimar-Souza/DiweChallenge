498bf874f5e57e5aed245dd39a1033e9ed3782f4
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-28 16:26:31 -0300

    Added public and private routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import PublicRoutes from './Routes/publicRoutes';
+import PrivateRoutes from './Routes/privateRoutes';
 import Pages from './pages';
 
 const App = () => {
   return (
     <Routes>
       <Route exact path='/' element={ <Navigate to='/login' /> } />
-      <Route exact path='/login' element={ <Pages.LoginPage /> } />
+      <Route exact path='/login' element={ <PublicRoutes /> }>
+        <Route exact path='/login' element={ <Pages.LoginPage /> } />
+      </Route>
+
+      <Route exact path='/contacts' element={ <PrivateRoutes /> }>
+        <Route exact path='/contacts' element={ <Pages.ContactsPage /> } />
+      </Route>
     </Routes>
   );
 }
