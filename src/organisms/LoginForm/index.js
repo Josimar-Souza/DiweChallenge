1587800b9b94bca2ab45ac9cab447e7c4e5a2a94
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-28 14:46:17 -0300

    Created method to save userCredentials

--- src/organisms/LoginForm/index.js
+++ src/organisms/LoginForm/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginFormStyle from './loginFormStyle';
 import Email from '../../molecules/Email';
 import Password from '../../molecules/Password';
 import Button from '../../atoms/Button';
 
 const LoginForm = () => {
+  const [userCredentials, setUserCredentials] = useState({ email: '', password: '' });
+
+  const onInputChange = ({ target: { name, value } }) => {
+    setUserCredentials({ ...userCredentials, [name]: value });
+  };
+
   return (
     <LoginFormStyle>
-      <Email/>
-      <Password />
+      <Email
+        name='email'
+        value={ userCredentials.email }
+        onChange={ onInputChange }
+      />
+      <Password
+        name='password'
+        value={ userCredentials.password }
+        onChange={ onInputChange }
+      />
       <Button>Fazer login</Button>
     </LoginFormStyle>
   );
