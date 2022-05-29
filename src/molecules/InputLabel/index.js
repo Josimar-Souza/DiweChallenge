fb10ccfa20e6404781b2ddfe4ffbe914e4a08f73
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-29 17:09:53 -0300

    Change space to tab in InputLabel

--- src/molecules/InputLabel/index.js
+++ src/molecules/InputLabel/index.js
@@ -5,48 +5,48 @@ import Paragraph from '../../atoms/Paragraph';
 import Input from '../../atoms/Input';
 
 const InputLabel = (props) => {
-  const {
-    width,
-    height,
-    onChange,
-    value,
-    name,
+	const {
+		width,
+		height,
+		onChange,
+		value,
+		name,
 		placeHolder,
 		label,
 		type,
-  } = props;
-  return (
-    <InputLabelStyle
-      width={ width }
-      height={ height }
-    >
-      <Paragraph>{ label }</Paragraph>
-      <Input
-        placeHolder={ placeHolder }
-        onChange={ onChange }
-        value={ value }
-        type={ type }
-        name={ name }
-      />
-    </InputLabelStyle>
-  );
+	} = props;
+	return (
+		<InputLabelStyle
+			width={ width }
+			height={ height }
+		>
+			<Paragraph>{ label }</Paragraph>
+			<Input
+				placeHolder={ placeHolder }
+				onChange={ onChange }
+				value={ value }
+				type={ type }
+				name={ name }
+			/>
+		</InputLabelStyle>
+	);
 }
 
 InputLabel.defaultProps = {
-  width: '100%',
-  height: 'fit-content',
-  name: '',
+	width: '100%',
+	height: 'fit-content',
+	name: '',
 	placeHolder: 'placeHolder',
 	label: 'Label',
 	type: 'text',
 }
 
 InputLabel.propTypes = {
-  width: PropTypes.string,
-  height: PropTypes.string,
-  onChange: PropTypes.func.isRequired,
-  value: PropTypes.string.isRequired,
-  name: PropTypes.string,
+	width: PropTypes.string,
+	height: PropTypes.string,
+	onChange: PropTypes.func.isRequired,
+	value: PropTypes.string.isRequired,
+	name: PropTypes.string,
 	placeHolder: PropTypes.string,
 	label: PropTypes.string,
 	type: PropTypes.string,
