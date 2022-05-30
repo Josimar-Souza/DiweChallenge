ce2507b3d8421a18d4dcf1cc044ff7b18a9a9c12
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-30 11:29:31 -0300

    Added structure for UpdateContactForm organism

--- src/organisms/UpdateContactForm/index.js
+++ src/organisms/UpdateContactForm/index.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import UpdateContactFormStyle from './updateContactFormStyle';
+import styles from './updateContactFormStyle';
 import Title from '../../atoms/Title';
+import Paragraph from '../../atoms/Paragraph';
+import Button from '../../atoms/Button';
+import InputLabel from '../../molecules/InputLabel';
 import ContactsAPI from '../../api/contactsAPI';
 import getLocalStorageItems from '../../utils/getLocalStorageItems'
 
@@ -10,6 +13,7 @@ const baseURL = process.env.REACT_APP_BASE_URL;
 const UpdateContactForm = ({ contactId }) => {
 	const [contactInfo, setContactInfo] = useState({ name: '', email: '', mobile: '' });
 	const contactsAPI = new ContactsAPI(baseURL, 10000);
+	const { UpdateContactFormStyle, EmailPhoneContainer } = styles;
 
 	useEffect(() => {
 		const getContact = async () => {
@@ -23,7 +27,37 @@ const UpdateContactForm = ({ contactId }) => {
 
 	return (
 		<UpdateContactFormStyle>
-			<Title>{`Atualize o contato ${contactInfo.name}`}</Title>
+			<Title textColor='blue'>{`Atualize o contato de ${contactInfo.name}`}</Title>
+			<Paragraph
+				fontSize='20px'
+			>
+				Altere as informações para atualizar o contato!
+			</Paragraph>
+			<InputLabel
+				label='Nome Completo'
+				value={ contactInfo.name }
+				width='90%'
+			/>
+			<EmailPhoneContainer>
+				<InputLabel
+					label='Email'
+					value={ contactInfo.email }
+					width='45%'
+					type='email'
+				/>
+				<InputLabel
+					label='Celular'
+					value={ contactInfo.mobile }
+					width='45%'
+				/>
+			</EmailPhoneContainer>
+			<Button
+				width='90%'
+				margin='30px 0'
+				scaleAnimation='scale(1.1, 1.1)'
+			>
+				Atualizar contato
+			</Button>
 		</UpdateContactFormStyle>
 	);
 };
