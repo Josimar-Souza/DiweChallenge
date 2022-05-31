2fa001f6b687f305f1f166abde1477c5558b9eb5
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-31 16:22:43 -0300

    Added MobileContactListItem structure

--- src/molecules/MobileContactListItem/index.js
+++ src/molecules/MobileContactListItem/index.js
@@ -1,12 +1,76 @@
 import React from 'react';
-import MobileContactListItemStyle from './mobileContactListItemStyle';
+import PropTypes from 'prop-types';
+import styles from './mobileContactListItemStyle';
+import Paragraph from '../../atoms/Paragraph';
+import phoneIcon from '../../Images/Icons/smartphone.svg';
 
-const MobileContactListItem = () => {
+const MobileContactListItem = ({ contact }) => {
+	const {
+		MobileContactListItemStyle,
+		UserInfoContainer,
+		NameEmailContainer,
+		MobilePhoneContainer,
+		PhoneIcon,
+	} = styles;
+	
+	const getPhoneNumber = (number) => {
+		let formattedNumber = '';
+
+		for (let index = 0; index < number.length; index += 1) {
+			if (index == 0) {
+				formattedNumber += `(${number[index]}`;
+			} else if (index == 1) {
+				formattedNumber += `${number[index]}) `;
+			}
+
+			if (number.length == 11 && index > 1) {
+				if (index < 7) {
+					formattedNumber += number[index];
+				} else if (index == 7) {
+					formattedNumber += `-${number[index]}`;
+				} else {
+					formattedNumber += number[index];
+				}
+			} else if (number.length < 11 && index > 1) {
+				formattedNumber += number[index];
+			}
+		}
+
+		return formattedNumber;
+	};
+
 	return (
 		<MobileContactListItemStyle>
-			Item
+			<UserInfoContainer>
+				<NameEmailContainer>
+					<Paragraph
+						textColor='balck'
+						mobileFontSize='2rem'>
+							{ contact.name }
+						</Paragraph>
+					<Paragraph
+						mobileFontSize='1.2rem'>
+							{ contact.email }
+						</Paragraph>
+				</NameEmailContainer>
+				<MobilePhoneContainer>
+					<PhoneIcon src={ phoneIcon } />
+					<Paragraph
+						mobileFontSize='1.2rem'>
+							{ getPhoneNumber(contact.mobile) }
+						</Paragraph>
+					</MobilePhoneContainer>
+			</UserInfoContainer>
 		</MobileContactListItemStyle>
 	);
 }
+
+MobileContactListItem.propTypes = {
+	contact: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		email: PropTypes.string.isRequired,
+		mobile: PropTypes.string.isRequired,
+	}).isRequired,
+};
 
 export default MobileContactListItem;
--- src/molecules/MobileContactListItem/mobileContactListItemStyle.js
+++ src/molecules/MobileContactListItem/mobileContactListItemStyle.js
@@ -1,7 +1,43 @@
 import styled from 'styled-components';
 
-const MobileContactListItemStyle = styled.div`
-
+const PhoneIcon = styled.img`
+	width: 18%;
 `;
 
-export default MobileContactListItemStyle;
+const MobilePhoneContainer = styled.div`
+	display: flex;
+	justify-content: space-around;
+	width: 100%;
+`;
+
+const NameEmailContainer = styled.div`
+	align-items: flex-start;
+	display: flex;
+	flex-direction: column;
+	margin-bottom: 25px;
+`;
+
+const UserInfoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 60%;
+`;
+
+const MobileContactListItemStyle = styled.div`
+	border: 1px solid gray;
+	border-radius: 6px;
+	display: flex;
+	justify-content: space-between;
+	padding: 10px;
+	width: 85%;
+`;
+
+const styles = {
+	MobileContactListItemStyle,
+	UserInfoContainer,
+	NameEmailContainer,
+	MobilePhoneContainer,
+	PhoneIcon,
+}
+
+export default styles;
