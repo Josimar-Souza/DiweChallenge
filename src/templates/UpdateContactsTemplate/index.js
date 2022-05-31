117e63804e0852a7c7c7fc2eca5c8dc05c321059
Josimar Souza <contact.josimarsouza@gmail.com>
2022-05-31 13:59:40 -0300

    Added MobileHeader to UpdateContactsTemplate

--- src/templates/UpdateContactsTemplate/index.js
+++ src/templates/UpdateContactsTemplate/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { isMobile } from 'react-device-detect';
 import UpdateContactsTemplateStyle from './updateContactsTemplateStyle';
 import Header from '../../organisms/Header';
+import MobileHeader from '../../organisms/MobileHeader';
 import UpdateContactForm from '../../organisms/UpdateContactForm';
 
 const UpdateContactsTemplate = ({ contactId }) => {
+	const getHeader = () => {
+		if (!isMobile) {
+			return (
+				<Header backPath='/contacts' />
+			);
+		}
+		
+		return (
+			<MobileHeader
+				backPath='/contacts'
+				title='Atualizar contato'
+				mobileFontSize='1.6rem'
+				titleMargin='0 0 0 2rem'
+			/>
+		);
+	}
+	
 	return (
 		<UpdateContactsTemplateStyle>
-			<Header backPath='/contacts' />
+			{ getHeader() }
 			<UpdateContactForm contactId={ contactId } />
 		</UpdateContactsTemplateStyle>
 	);
